35689e1677101dbd62369d7bc1394ca22dd6135e
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2018-06-13 11:41:38 +0200

    [Chore] Remove todo comments

--- src/lib/wp/getWPCategories.js
+++ src/lib/wp/getWPCategories.js
@@ -1,7 +1,6 @@
 import makeRequest from '../makeRequest'
 
 export default function getWPCategories (config, lang, options = {}) {
-  // TODO: Check if categories can be fetched by language as well
   return makeRequest(
     config, 
     '/wp/v2/categories'
